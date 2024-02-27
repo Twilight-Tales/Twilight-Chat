9ae637c84233e7ada2f66e9daa5becc9b59d644e
Hannah Zhou <hannahyunzizhou@gmail.com>
2024-02-26 21:20:48 -0500

    edited backend relationships and fields

--- bookclub_backend.py
+++ bookclub_backend.py
@@ -45,15 +45,14 @@ class DatabaseDriver(object):
 
     def create_user_table(self):
         """Create a database of users
+            no explicit relationship
         """
         try:
             self.c.execute('''
             CREATE TABLE IF NOT EXISTS Users (
                 user_id TEXT PRIMARY KEY,
                 username TEXT NOT NULL,
-                password TEXT NOT NULL,
-                ch_id TEXT,
-                FOREIGN KEY (ch_id) REFERENCES ChatHistory(ch_id)
+                password TEXT NOT NULL
             );
         ''')
         except Exception as e:
@@ -62,24 +61,28 @@ class DatabaseDriver(object):
 
     def create_chapter_table(self):
         """
+        Create a table of Chapters
+        explicit: Book (one to many) Chapters
         """
         try:
             self.c.execute('''
-            CREATE TABLE IF NOT EXISTS Chapter(
+            CREATE TABLE IF NOT EXISTS Chapters(
                 chapter_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 chapter_number INTEGER NOT NULL,
                 chapter_title TEXT NOT NULL,
                 chapter_text TEXT NOT NULL,
+                book_id INTEGER NOT NULL,
                 FOREIGN KEY (book_id) REFERENCES Books(book_id)
             );
             ''')
         except Exception as e:
-            print("Error creating table: ", e)
+            print("Error creating Chapters table: ", e)
             raise e
 
     def create_book_library_table(self):
         """
         Create the books table if it doesn't exist
+        Many to many relationship with Book reading histories
         
         """
         try:
@@ -88,41 +91,44 @@ class DatabaseDriver(object):
                 book_id INTEGER PRIMARY KEY AUTOINCREMENT, 
                 author TEXT NOT NULL,
                 book_title TEXT NOT NULL,
-                pages INTEGER NOT NULL,
-                chapter_id INTEGER,
-                FOREIGN KEY (chapter_id) REFERENCES Chapter(chapter_id)
+                pages INTEGER NOT NULL
                 );
             ''')
         except Exception as e:
-            print("Error creating table: ", e)
+            print("Error creating Books table: ", e)
             raise e
         
     def create_chat_history_table(self):
         """
         Create a chat history table
+        User(one to many) Chat Histories
+        BookReadingHistories(one to one)Chat Histories
         """
         #Chat History table
         try:
             self.c.execute(
-            '''CREATE TABLE IF NOT EXISTS ChatHistory (
+            '''CREATE TABLE IF NOT EXISTS ChatHistories (
                 ch_id INTEGER PRIMARY KEY,
                 time DATETIME NOT NULL,
                 reading_id INTEGER NOT NULL UNIQUE,
-                FOREIGN KEY (reading_id) REFERENCES BookReadingHistory (reading_id)
+                FOREIGN KEY (reading_id) REFERENCES BookReadingHistories (reading_id),
+                user_id TEXT NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES Users(user_id)
             );'''
             )
         except Exception as e:
-            print("Error creating table: ", e)
+            print("Error creating chat histories table: ", e)
             raise e
         
     def create_book_history_table(self):
         """
         Book Reading History table
+        Many to Many relationship with books
         """
         
         try:
             self.c.execute(
-            '''CREATE TABLE IF NOT EXISTS BookReadingHistory  (
+            '''CREATE TABLE IF NOT EXISTS BookReadingHistories  (
                 reading_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 book_title TEXT NOT NULL,
                 current_chapter INTEGER NOT NULL,
@@ -131,7 +137,7 @@ class DatabaseDriver(object):
             );
             ''')
         except Exception as e:
-            print("Error creating table: ", e)
+            print("Error creating Book Histories table: ", e)
             raise e
 
     def create_library_history_assoctable(self):
@@ -146,11 +152,11 @@ class DatabaseDriver(object):
                     book_id INTEGER NOT NULL,
                     reading_id INTEGER NOT NULL,
                     FOREIGN KEY (book_id) REFERENCES Books(book_id),
-                    FOREIGN KEY (reading_id) REFERENCES BookReadingHistory(reading_id)
+                    FOREIGN KEY (reading_id) REFERENCES BookReadingHistories(reading_id)
                 );'''
             )
         except Exception as e:
-            print("Error creating table: ", e)
+            print("Error creating Association table: ", e)
             raise e
     
     def get_all_books(self):
